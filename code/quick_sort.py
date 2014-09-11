1fafc2331d7964bf478f8e0d156ce3134c7de8e1
Liang <ryanrhymes@gmail.com>
2014-09-12 00:31:17 +0300

    test

---
+++ code/quick_sort.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# Quick sort algorithm
+# time complexity is O(n*log(n))
+# worst case is O(n^2)
+#
+# Liang Wang @ University of Helsinki, Finland
+# 2014.09.11
+#
+
+def quick_sort(L, p, r):
+    if p < r:
+        q = partition(L, p, r)
+        quick_sort(L, p, q-1)
+        quick_sort(L, q+1, r)
+    return L
+
+
+def partition(A):
+    pass
