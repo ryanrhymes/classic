1f80df2c734ffe3f2e85d2a3d82acb7047989e02
Liang <ryanrhymes@gmail.com>
2014-09-13 17:39:59 +0300

    tree start

---
+++ code/tree.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# Different tree algorithms
+# 
+# Liang Wang @ University of Helsinki, Finland
+# 2014.09.13
+#
+
+class Node():
+    left  = None
+    right = None
+    value = None
+    pass
+
+
+class BinaryTree():
+    root = None
+
+    
+    pass
---
+++ test/test_trees.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# This script is used for testing tree algorithms.
+#
+# Liang Wang @ University of Helsinki, Finland
+# 2014.09.13
+#
+
+import os
+import sys
+import random
+
+sys.path.append('../code')
+
+
+def get_random_tree():
+    return
+
+
+def main():
+
+    G = get_random_tree()
+
+    print 'Test Binary Tree', '-'*20
+
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
