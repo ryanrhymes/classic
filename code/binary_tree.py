361b40ddf169d36881f0a21b4094a89197fad24b
Liang <ryanrhymes@gmail.com>
2014-09-13 18:34:55 +0300

    coding

--- code/binary_tree.py
+++ code/binary_tree.py
@@ -6,15 +6,49 @@
 # 2014.09.13
 #
 
+import sys
+
+
 class Node():
     left  = None
     right = None
     value = None
+
+    def __init__(self, v=None):
+        self.value = v
     pass
 
 
 class BinaryTree():
     root = None
 
-    
+    def search(self, key):
+        node = self.root
+        while node != None and node.value != key:
+            if key < node.value:
+                node = node.left
+            else:
+                node = node.right
+        return node
+
+    def inorder_walk(self, node):
+        if node is not None:
+            self.inorder_walk(node.left)
+            sys.stdout.write("%i " % node.value)
+            self.inorder_walk(node.right)
+
+
+    def preorder_walk(self, node):
+        if node is not None:
+            sys.stdout.write("%i " % node.value)
+            self.inorder_walk(node.left)
+            self.inorder_walk(node.right)
+
+
+    def postorder_walk(self, node):
+        if node is not None:
+            self.inorder_walk(node.left)
+            self.inorder_walk(node.right)
+            sys.stdout.write("%i " % node.value)
+
     pass
--- test/test_trees.py
+++ test/test_trees.py
@@ -13,16 +13,24 @@ import random
 sys.path.append('../code')
 
 
-def get_random_tree():
-    return
+def get_random_binary_tree():
+    from binary_tree import BinaryTree, Node
+    G = BinaryTree()
+    G.root = Node(5)
+    G.root.left = Node(2); G.root.right = Node(7)
+    G.root.left.left = Node(1); G.root.left.right = Node(4)
+    G.root.right.right = Node(8)
+    return G
 
 
 def main():
 
-    G = get_random_tree()
-
+    G = get_random_binary_tree()
     print 'Test Binary Tree', '-'*20
-
+    G.inorder_walk(G.root); print '';
+    G.preorder_walk(G.root); print '';
+    G.postorder_walk(G.root); print '';
+    print G.search(4).value
 
     pass
 
